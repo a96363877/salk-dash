f972aac13a1cfaf38fd9ea2a594d05c385302be0
a96363877 <a96363877@gmail.com>
2025-04-21 08:51:33 +0300

    dsff

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -4,15 +4,14 @@ import { getFirestore} from 'firebase/firestore';
 import { getDatabase} from 'firebase/database';
 
 const firebaseConfig = {
-   apiKey: "AIzaSyAcnAAlZzpKwN0nwNTzsrz0ka8OleoljJg",
+  apiKey: "AIzaSyAcnAAlZzpKwN0nwNTzsrz0ka8OleoljJg",
   authDomain: "mysms-7b5e8.firebaseapp.com",
   databaseURL: "https://mysms-7b5e8-default-rtdb.firebaseio.com",
   projectId: "mysms-7b5e8",
   storageBucket: "mysms-7b5e8.firebasestorage.app",
   messagingSenderId: "1008260512816",
   appId: "1:1008260512816:web:824a72a0a63c1dc229c1f5",
-  measurementId: "G-PLK4GEZFSZ"
-};
+  measurementId: "G-PLK4GEZFSZ"};
 
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
